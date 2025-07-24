b445f7e62d5d5177ce72c7e64b4c44d32161d759
DESKTOP-H87TPR2\ASUS <joystonf@gmail.com>
2025-07-24 17:49:02 +0530

    -Implemented code to get multiple Albums by name

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+type Album struct {
+	Id     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 var db *sql.DB
 
 func main() {
@@ -33,4 +40,36 @@ func main() {
 
 	}
 	fmt.Println("Connected")
+
+	albums, err := getAlbumsbyArtist("John Coltrane")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found %v\n", albums)
+}
+
+func getAlbumsbyArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("Select * from album where artist=?", name)
+	if err != nil {
+		return nil, fmt.Errorf("getAlbumsbyArtist %q: %v", name, err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.Id, &alb.Artist, &alb.Title, &alb.Price); err != nil {
+			return nil, fmt.Errorf("getAlbumsbyArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+
+	//Check for error from overall query
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("getAlbumsbyArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
